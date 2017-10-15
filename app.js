6d27a702597bf079c8d15da2eef9b994b8603891
Makoto Ito <3mdevmail@gmail.com>
2017-10-15 14:43:50 +0900

    :books: 色々追加
    設定用ファイルの例 /
     Botの基幹部を追加
    .gitignoreに設定ファイル本体を追加

---
+++ app.js
@@ -0,0 +1,25 @@
+const Discord = require('discord.js');
+const client = new Discord.Client();
+const settings = require('./settings.json')
+const p = settings.prefix;
+var time = new Date;
+
+client.on('ready', () => {
+  console.log('OK!');
+  client.user.setGame(settings.gameName);
+});
+
+client.on('message', m => {
+  const r = ('<@' + m.author.id + '>  ');
+  if (m.author.bot) return;
+  else {
+    if (m.content === p + 'invite') {
+      m.channel.send('https://discord.gg/pcH6z9p');
+    };
+    if (m.content === p + 'ping') {
+      m.channel.send(r + 'pong  ' + `${Math.round(client.ping)}` + 'ms');
+    }
+  };
+});
+
+client.login(settings.DiscordToken);
