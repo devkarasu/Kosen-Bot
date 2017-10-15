4513275885c7cbd18f05179ceaee0c8d958929bd
Makoto Ito <3mdevmail@gmail.com>
2017-10-15 15:15:18 +0900

    :pencil: helpコマンドの実装

--- app.js
+++ app.js
@@ -4,6 +4,41 @@ const settings = require('./settings.json')
 const p = settings.prefix;
 var time = new Date;
 
+const help_embed = {
+  "title": "This Bot Help",
+  "url": "https://github.com/mak0tia/proconist_bot",
+  "color": 5938152,
+  "thumbnail": {
+    "url": "https://files.3mdev.space/pictures/icon/procon_logo.jpg"
+  },
+  "author": {
+    "name": "author name",
+    "url": "https://discordapp.com",
+    "icon_url": "https://cdn.discordapp.com/embed/avatars/0.png"
+  },
+  "fields": [{
+      "name": "help",
+      "value": "このメニューが表示されます"
+    },
+    {
+      "name": "invite",
+      "value": "このDiscordチャンネルへの招待リンクを発行します"
+    },
+    {
+      "name": "ping",
+      "value": "自分からサーバへのPingを表示します\n今後任意のサーバへのPingを実装予定です"
+    },
+    {
+      "name": "BotGit",
+      "value": "このBotのGitHubのURLを投げます\nIssueやPull Request等待っています"
+    },
+    {
+      "name": "その他",
+      "value": "その他要望等ありましたらMakotiaの[Twitter](https://twitter.com/hs6a)やDiscord、Issueなどどうぞ"
+    }
+  ]
+};
+
 client.on('ready', () => {
   console.log('OK!');
   client.user.setGame(settings.gameName);
@@ -22,6 +57,12 @@ client.on('message', m => {
     if (m.content === p + 'BotGit') {
       m.reply(settings.gitURL);
     };
+    if (m.content === p + 'help') {
+      m.channel.send('すべてのコマンドの前には' + p + 'をつけてください')
+      m.channel.send({
+        help_embed
+      });
+    }
   };
 });
 
