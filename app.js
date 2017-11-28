67160bea9ac9c1dc11ab9a895fcc0f9169672b53
Makoto Ito <3mdevmail@gmail.com>
2017-11-29 01:30:54 +0900

    :books: ESLintの設定を追加、警告修正

--- app.js
+++ app.js
@@ -1,40 +1,43 @@
-const Discord = require('discord.js');
+const Discord = require("discord.js");
 const client = new Discord.Client();
-const settings = require('./settings.json')
+var fs = require("fs");
+var settings = JSON.parse(fs.readFileSync("./setting.json", "utf8"));
 const p = settings.prefix;
-var time = new Date;
 
-
-client.on('ready', () => {
-  console.log('OK!');
+client.on("ready", () => {
+  console.log("OK!");
   client.user.setGame(settings.gameName);
 });
 
-client.on('message', m => {
-  const r = ('<@' + m.author.id + '>  ');
+client.on("message", m => {
+  const r = ("<@" + m.author.id + ">  ");
   if (m.author.bot) return;
   else {
-    if (m.content === p + 'invite') {
-      m.channel.send('https://discord.gg/pcH6z9p');
-    };
-    if (m.content === p + 'ping') {
-      m.channel.send(r + 'pong  ' + `${Math.round(client.ping)}` + 'ms');
-    };
-    if (m.content === p + 'BotGit') {
-      m.reply(settings.gitURL);
-    };
-    if (m.content === p + 'help') {
-      m.channel.send('```\n' + p + 'help\n``` \n' +
-        'このメニューが表示されます\n' +
-        '```\n' + p + 'invite\n``` \n' +
-        'このDiscordチャンネルへの招待リンクを発行します\n' +
-        '```\n' + p + 'ping\n```\n' +
-        '自分からサーバへのPingを表示します\n今後任意のサーバへのPingを実装予定です' +
-        '```\n' + p + 'BotGit\n```\n' +
-        'このBotのGitHubのURLを投げます\nIssueやPull Request等待っています\n' +
-        'その他\nその他要望等ありましたらMakotiaのTwitter(@hs6a)やDiscord(Makotia#2337)、Issueなどどうぞ');
+    if (m.content === p + "invite") {
+      m.channel.send("https://discord.gg/pcH6z9p");
     }
-  };
+
+    if (m.content === p + "ping") {
+      m.channel.send(r + "pong  " + `${Math.round(client.ping)}` + "ms");
+    }
+
+    if (m.content === p + "BotGit") {
+      m.reply(settings.gitURL);
+    }
+
+    if (m.content === p + "help") {
+      m.channel.send("```\n" + p + "help\n``` \n" +
+        "このメニューが表示されます\n" +
+        "```\n" + p + "invite\n``` \n" +
+        "このDiscordチャンネルへの招待リンクを発行します\n" +
+        "```\n" + p + "ping\n```\n" +
+        "自分からサーバへのPingを表示します\n今後任意のサーバへのPingを実装予定です" +
+        "```\n" + p + "BotGit\n```\n" +
+        "このBotのGitHubのURLを投げます\nIssueやPull Request等待っています\n" +
+        "その他\nその他要望等ありましたらMakotiaのTwitter(@hs6a)やDiscord(Makotia#2337)、Issueなどどうぞ");
+    }
+  }
+
 });
 
 client.login(settings.DiscordToken);
